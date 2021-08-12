12b61fc5ffd63e45ab12d96fb4dd995c69042861
Daniel Sato <danielsatto@hotmail.com>
2021-08-12 20:21:58 -0300

    Refatorado

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using webapi.Data;
 
@@ -20,10 +13,7 @@ namespace webapi
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
-            //Alteração git  
-            
-                      
+            Configuration = configuration;       
         }
 
         public IConfiguration Configuration { get; }
